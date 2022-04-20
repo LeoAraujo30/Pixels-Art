30154f90f940cb365fc3440a5f4dc8c941dfe017
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-04-20 16:45:05 -0300

    requisitos 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,8 @@
+function paletaDeCores() {
+     const cor = document.getElementsByClassName('color');
+     cor[0].style.background = 'black'
+     cor[1].style.background = 'red'
+     cor[2].style.background = 'green'
+     cor[3].style.background = 'blue'
+}
+paletaDeCores()
