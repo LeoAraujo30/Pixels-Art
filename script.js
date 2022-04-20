caa882493e263228f971b6344a8c045d56856a11
Leonardo Araujo de Paulo <leonardo.araujo300301@gmail.com>
2022-04-20 19:25:59 -0300

    requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function paletaDeCores() {
-     const cor = document.getElementsByClassName('color');
+function CoresDaPaleta() {
+    const cor = document.getElementsByClassName('color');
      cor[0].style.background = 'black'
      cor[1].style.background = 'red'
      cor[2].style.background = 'green'
      cor[3].style.background = 'blue'
 }
-paletaDeCores()
+CoresDaPaleta()
+
+
+const paleta1 = document.getElementsByTagName('li')[0];
+const paleta2 = document.getElementsByTagName('li')[1];
+const paleta3 = document.getElementsByTagName('li')[2];
+const paleta4 = document.getElementsByTagName('li')[3];
+ function paletaSelecionada(event) {
+    const selected = document.querySelector('.selected');
+    selected.classList.remove('selected')
+    event.target.classList.add('selected')
+}
+paleta1.addEventListener('click', paletaSelecionada)
+paleta2.addEventListener('click', paletaSelecionada)
+paleta3.addEventListener('click', paletaSelecionada)
+paleta4.addEventListener('click', paletaSelecionada)
